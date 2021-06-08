56be47b8e77ce4f6bf5ea1c5c66ecbe745d50c7a
David Venhoek <david@tweedegolf.com>
2021-06-08 09:09:20 +0200

    Added implementation of session clearing.

--- schema.sql
+++ schema.sql
@@ -1,14 +1,16 @@
-DROP TABLE IF EXISTS "session";
+DROP TABLE IF EXISTS session;
 
-CREATE TABLE "session" (
-    "id" SERIAL NOT NULL,
-    "dtmfcode" text NOT NULL,
-    "resultcode" text NOT NULL,
-    "sessionid" text,
-    "purpose" text,
-    "attr_jwt" text,
-    PRIMARY KEY ("id")
+CREATE TABLE session (
+    id SERIAL NOT NULL,
+    dtmfcode text NOT NULL,
+    resultcode text NOT NULL,
+    lastActivity timestamp NOT NULL,
+    sessionid text,
+    purpose text,
+    attr_jwt text,
+    PRIMARY KEY (id)
 );
 
-CREATE UNIQUE INDEX ON "session" ("dtmfcode");
-CREATE UNIQUE INDEX ON "session" ("sessionid");
+CREATE UNIQUE INDEX ON session (dtmfcode);
+CREATE UNIQUE INDEX ON session (sessionid);
+CREATE INDEX ON session (lastActivity);
